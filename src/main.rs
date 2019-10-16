6b0a481f63234c66223a482d2b123557ad0a60b5
heydabop <heydabop@gmail.com>
2019-10-16 02:11:50 -0500

    get config from toml file

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,33 @@ use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Expected: {} <IP address> <port>", args[0]);
+    if args.len() != 2 {
+        eprintln!("Usage: {} <config.toml>", args[0]);
+        std::process::exit(1);
     }
-    let ip = &args[1];
-    let port = &args[2];
 
-    let mut stream = TcpStream::connect(format!("{}:{}", ip, port))?;
+    let config = std::fs::read_to_string(&args[1])?
+        .parse::<toml::Value>()
+        .unwrap();
+
+    let broker_addr = config["broker_addr"].as_str().unwrap();
+
+    let client_id = config["client_id"].as_str().unwrap();
+    if client_id.len() > 0xFF {
+        panic!("Client ID too long");
+    }
+
+    let username = config["username"].as_str().unwrap();
+    if username.len() > 0xFF {
+        panic!("Username too long");
+    }
+
+    let password = config["password"].as_str().unwrap();
+    if password.len() > 0xFF {
+        panic!("Password too long");
+    }
+
+    let mut stream = TcpStream::connect(broker_addr)?;
     stream.set_read_timeout(None)?;
     stream.set_nodelay(true)?;
 
@@ -26,23 +46,23 @@ fn main() -> std::io::Result<()> {
     connect_msg.push(10); // keep alive 10 seconds
 
     connect_msg.push(0); // client ID len
-    connect_msg.push(8); // client ID len
-    for b in "humidity".chars() {
+    connect_msg.push(client_id.len() as u8); // client ID len
+    for b in client_id.chars() {
         // client ID
         connect_msg.push(b as u8);
     }
     // no will topic or will message
 
     connect_msg.push(0); // username length
-    connect_msg.push(8); // username length
-    for b in "humidity".chars() {
+    connect_msg.push(username.len() as u8); // username length
+    for b in username.chars() {
         // username
         connect_msg.push(b as u8);
     }
 
     connect_msg.push(0); // password length
-    connect_msg.push(10); // passowrd length
-    for b in "__________".chars() {
+    connect_msg.push(password.len() as u8); // passowrd length
+    for b in password.chars() {
         // password
         connect_msg.push(b as u8);
     }
